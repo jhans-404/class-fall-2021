a8e64fa655e35311b2371ddbedb6d935983a4d64
Joshua Hans <jhans@schools.nyc.gov>
2021-11-01 08:36:02 -0400

    crows distance working

--- classcode/coordinate-plane/CoordinatePlane.java
+++ classcode/coordinate-plane/CoordinatePlane.java
@@ -10,9 +10,15 @@ public class CoordinatePlane {
     String point1 = s.nextLine();
 
     int x1 = getX(point1);
-    System.out.println(x1);
+    int y1 = getY(point1);
 
-    System.out.println(x1 + 2);
+    System.out.print("please input another coordinate: ");
+    String point2 = s.nextLine();
+
+    int x2 = getX(point2);
+    int y2 = getY(point2);
+
+    crowsDistance(x1, y1, x2, y2);
   }// end main method
 
   /*
@@ -24,6 +30,7 @@ public class CoordinatePlane {
   */
   public static int getX(String point) {
     int commaPos = point.indexOf(",");
+
     // get the String version of x
     String xVal = point.substring(1, commaPos);
 
@@ -35,12 +42,34 @@ public class CoordinatePlane {
   }
 
   public static int getY(String point) {
+    int commaPos = point.indexOf(",");
 
-    return 0;
+    // get the String version of y
+    String yVal = point.substring(commaPos + 2, point.length() - 1);
+
+    // convert that String to an int
+    int y = Integer.parseInt(yVal);
+
+    // send the info back to main
+    return y;
   }
 
   public static void crowsDistance(int x1, int y1, int x2, int y2) {
+    // distance formula = sqrt[(y2 - y1)^2 + (x2 - x1)^2]
+    int deltaX = x2 - x1;
+    int deltaY = y2 - y1;
 
+    // distance formula = sqrt[(deltaY)^2 + (deltaX)^2]
+    int deltaXsquared = deltaX * deltaX;
+    int deltaYsquared = deltaY * deltaY;
+
+    // distance formula = sqrt[(deltaYsquared + deltaXsquared]
+    int sum = deltaYsquared + deltaXsquared;
+
+    // distance formula = sqrt[sum]
+    double distance = Math.sqrt(sum);
+
+    System.out.println("The distance between the two points is: " + distance);
   }// end crowsDistance method
 
   public static void taxicabDistance(int x1, int y1, int x2, int y2) {
