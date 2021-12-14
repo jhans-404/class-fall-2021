fafb9d1998bd2fa9e1760be11a7571a24d3cd801
Joshua Hans <jhans@schools.nyc.gov>
2021-12-14 08:46:41 -0500

    need to finalize mode

--- classcode/central-measures/CentralMeasures.java
+++ classcode/central-measures/CentralMeasures.java
@@ -20,8 +20,8 @@ public class CentralMeasures {
 
     System.out.println(Arrays.toString(randomArray));
     System.out.println(average(randomArray));
+    System.out.println(mode(testGrades));
   }
-
 
   public static double average(int[] arr) {
     // declare accumulator variable
@@ -34,5 +34,36 @@ public class CentralMeasures {
 
     // divide total by number of data values
     return total / arr.length;
-  }
-}
+  } // end average method
+
+  public static int mode(int[] arr) {
+    // find the max value
+    int max = findMax(arr);
+
+    // construct a new array for frequencies
+    // the size of the array will be max + 1
+    int[] freq = new int[max + 1];
+
+    for(int i = 0; i < arr.length; i++) {
+        freq[arr[i]]++;
+    }
+
+    // how do we get the max value's index to be returned?
+
+    return 0;
+  } // end mode method
+
+  public static int findMax(int[] arr) {
+    int max = -99999;
+
+    // {100, 95, 90, 95}
+    for(int i = 0; i < arr.length; i++) {
+      if(arr[i] > max) {
+        max = arr[i];
+      } // end if statement
+    } // end for loop
+
+    return max;
+  } // end findMax method
+
+} // end class
