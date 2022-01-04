1a83dbe770bb5af53a9aa2c7a59ee02b675f97fd
Joshua Hans <jhans@schools.nyc.gov>
2022-01-04 08:48:25 -0500

    adds another grade

--- classcode/grades-array/Exams.java
+++ classcode/grades-array/Exams.java
@@ -23,12 +23,38 @@ public class Exams {
 
     // display the first 5 grades
     System.out.println(Arrays.toString(sampleStudent));
+    System.out.println(Arrays.toString(addGrade(sampleStudent)));
   } // end main method
 
 /*
 A) Write a method that adds a 6th exam
 to the sample student’s set of grades.
 */
+public static int[] addGrade(int[] grades) {
+  // create a new array that is one size larger
+  int[] moreGrades = new int[grades.length + 1];
+
+  /*
+  // traverse grades and copy to the new array
+  for(int i = 0; i < grades.length; i++) {
+    // take the value from grades and assign to moreGrades in the same slot
+    moreGrades[i] = grades[i];
+  } // end for loop
+  */
+
+  // enhanced for loop:
+  int i = 0;
+  for(int grade : grades) { // read as: "for each grade in grades"
+    moreGrades[i] = grade;
+    i++;
+  }
+
+  // add the last grade to the last slot in the new array
+  moreGrades[moreGrades.length - 1] = (int)(Math.random() * 46) + 55;
+
+  return moreGrades;
+} // end addGrade
+
 
 /*
 B) Write a method that takes the original set
