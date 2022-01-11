53a6c9482687b16ec0027b9570ecf50e40cc6f1c
Joshua Hans <jhans@schools.nyc.gov>
2022-01-11 12:09:29 -0500

    initialize the 2D array

---
+++ classcode/grades-array2D/Grades2D.java
@@ -0,0 +1,27 @@
+public class Grades2D {
+  public static void main(String[] args) {
+    // initialize a 2-dimensional array
+    // 15 rows, each storing 5 values
+    /*
+    [
+      [[0][0], , , , ]
+      [ , , , ,[1][4]]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ ,[5][1], , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+      [ , , , , ]
+    ]
+    */
+    int[][] studentGrades = new int[15][5];
+  }
+
+}
