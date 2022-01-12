7f4938653b750a65285fb37ea646940b0181be3a
Joshua Hans <jhans@schools.nyc.gov>
2022-01-12 08:34:41 -0500

    filled, accessed and printed values

--- classcode/grades-array2D/Grades2D.java
+++ classcode/grades-array2D/Grades2D.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Grades2D {
   public static void main(String[] args) {
     // initialize a 2-dimensional array
@@ -10,18 +12,33 @@ public class Grades2D {
       [ , , , , ]
       [ , , , , ]
       [ ,[5][1], , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
-      [ , , , , ]
+      .
+      .
+      .
+      [[14][0],[14][1],[14][2],[14][3],[][]]
     ]
     */
     int[][] studentGrades = new int[15][5];
+
+    System.out.println(studentGrades[studentGrades.length-1][studentGrades[0].length - 1]);
+    System.out.println(studentGrades.length); // = 15
+    System.out.println(studentGrades[0].length); // = 5
+
+    int num = 1;
+    for(int row = 0; row < studentGrades.length; row++) {
+      for(int col = 0; col < studentGrades[0].length; col++) {
+        studentGrades[row][col] = num;
+        num++;
+      }
+    }
+    System.out.println(studentGrades[studentGrades.length-1][studentGrades[0].length - 1]);
+
+    // for(int array = 0; array < studentGrades.length; array++){
+    //   System.out.println(Arrays.toString(studentGrades[array]));
+    // }
+
+    System.out.println(Arrays.deepToString(studentGrades));
+
   }
 
 }
