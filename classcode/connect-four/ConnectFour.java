e44f8594f4c48b7fc51ff051a76993e5392165da
Joshua Hans <jhans@schools.nyc.gov>
2022-01-14 10:43:57 -0500

    some ANSI codes in, sample round

--- classcode/connect-four/ConnectFour.java
+++ classcode/connect-four/ConnectFour.java
@@ -6,10 +6,24 @@ public class ConnectFour {
 
     String[][] board = new String[6][7];
 
-    //System.out.println(Arrays.deepToString(board));
+    System.out.println("\u001B[32mConnect Four!\nPress enter to start.");
+    s.nextLine();
+
     fillBoard(board);
     displayBoard(board);
+
+    // example, player 1 chooses col 3
+    board[5][2] = "[X]";
+    s.nextLine();
+    displayBoard(board);
+
+    // player 2 also chooses col 3
+    board[4][2] = "[O]";
+    s.nextLine();
+    displayBoard(board);
+
   } // end main method
+
 
   public static void fillBoard(String[][] board) {
     for(int row = 0; row < board.length; row++) {
@@ -20,6 +34,7 @@ public class ConnectFour {
   } // end fillBoard method
 
   public static void displayBoard(String[][] board) {
+    System.out.println("\033[H\033[2J"); // clear the terminal
     for(int row = 0; row < board.length; row++) {
       for(int col = 0; col < board[row].length; col++) {
         System.out.print(board[row][col] + " ");
